20721deddde6a50eb64c52820324547bbb7401d4
Bruno Matos Silva <brunomatossilvait@gmail.com>
2025-02-17 20:30:16 -0300

    remove favicon and globals.css; add utility functions, login page, and button component; update tsconfig and package.json

---
+++ src/app/auth/login/page.tsx
@@ -0,0 +1,7 @@
+export default function Login() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1 className="text-primary font-bold text-2xl">Faça seu login na aplicação</h1>
+    </main>
+  );
+}
